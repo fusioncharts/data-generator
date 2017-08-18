7ad58ed032080fe62d125be6142f9d2ee1bb36c7
priyanjitdey94 <priyanjitcareer@gmail.com>
2017-08-18 18:09:45 +0530

    Integrated datagenerator with numbergenerator

--- src/datagenerator.js
+++ src/datagenerator.js
@@ -1,178 +1,212 @@
+const NumberGenerator = require('./numbergenerator.js');
+const numberGeneratorObj = new NumberGenerator();
 
-class DataGenerator{
-    constructor(str){
+class DataGenerator {
+    constructor(str) {
         this.inputAr = [];
-        this.inputDataSetAr=[];
-        this.inputDataScatter=[];
-        this.inputStringAr=[];
-        this.finalJSON={};
-        this.finalJSONAr=[];
+        this.inputDataSetAr = [];
+        this.inputDataScatter = [];
+        this.inputStringAr = [];
+        this.finalJSON = {};
+        this.finalJSONAr = [];
         // 0 - Data, 1 - Data, 2 - Scatter, 3 - Bubble
-        this.chartType=-1;
-        this.chartTypeZero=['bar2d','column2d','pie2d','line','area2d','doughnut2d','pareto2d'];
-        this.chartTypeOne=['mscolumn2d','msbar2d','msline','msarea','stackedcolumn2d','stackedarea2d','stackedbar2d'];
-        this.chartTypeTwo=['scatter'];
-        this.chartTypeThree=['bubble'];
+        this.chartType = -1;
+        this.chartTypeZero = ['bar2d', 'column2d', 'pie2d', 'line', 'area2d', 'doughnut2d', 'pareto2d'];
+        this.chartTypeOne = ['mscolumn2d', 'msbar2d', 'msline', 'msarea', 'stackedcolumn2d', 'stackedarea2d', 'stackedbar2d'];
+        this.chartTypeTwo = ['scatter'];
+        this.chartTypeThree = ['bubble'];
         this.setChartType(str);
     }
 
-    initialize(){
-        this.inputAr=[];
-        this.inputDataSetAr=[]
-        this.inputDataScatter=[];
-        this.inputStringAr=[];
-        this.finalJSON={};
-        this.finalJSONAr=[];
+    initialize() {
+        this.inputAr = [];
+        this.inputDataSetAr = []
+        this.inputDataScatter = [];
+        this.inputStringAr = [];
+        this.finalJSON = {};
+        this.finalJSONAr = [];
     }
 
-    presentIn(arr,target){
+    presentIn(arr, target) {
         let i;
-        for(i=0;i<arr.length;i++){
-            if(arr[i]===target){
+        for (i = 0; i < arr.length; i++) {
+            if (arr[i] === target) {
                 return true;
             }
         }
         return false;
     }
 
-    setChartType(str){
-        if(str===undefined){
+    setChartType(str) {
+        if (str === undefined) {
             return false;
         }
 
-        if(this.presentIn(this.chartTypeZero,str)){
-            this.chartType=0;
-        }else if(this.presentIn(this.chartTypeOne,str)){
-            this.chartType=1;
-        }else if(this.presentIn(this.chartTypeTwo,str)){
-            this.chartType=2;
-        }else if(this.presentIn(this.chartTypeThree,str)){
-            this.chartType=3;
-        }else{
-            return 'Chart type not supported';
+        if (this.presentIn(this.chartTypeZero, str)) {
+            this.chartType = 0;
+        } else if (this.presentIn(this.chartTypeOne, str)) {
+            this.chartType = 1;
+        } else if (this.presentIn(this.chartTypeTwo, str)) {
+            this.chartType = 2;
+        } else if (this.presentIn(this.chartTypeThree, str)) {
+            this.chartType = 3;
+        } else {
+            console.log('Chart type not supported.');
+            return false;
         }
-
+        numberGeneratorObj.chartType = this.chartType;
         return this.chartType;
     }
 
-    parseData(arr){
-        let i, tempJSONdata={};
+    parseData(arr) {
+        let i, tempJSONdata = {};
 
-        for(i=0;i<arr.length;i++){
-            tempJSONdata={};
-            tempJSONdata['label']='label';
-            tempJSONdata['value']=arr[i]+'';
+        for (i = 0; i < arr.length; i++) {
+            tempJSONdata = {};
+            tempJSONdata['label'] = 'label';
+            tempJSONdata['value'] = arr[i] + '';
             this.finalJSONAr.push(tempJSONdata);
         }
-        
+
         return this.finalJSONAr.length;
     }
 
-    parseDataset(arr,append){
-        let i, tempAr=[], tempObj={};
+    parseDataset(arr, append) {
+        let i, tempAr = [],
+            tempObj = {};
 
-        if(append===true){
-            if(this.finalJSONAr.length>0){
-                tempAr=this.finalJSONAr.pop().data;
+        if (append === true) {
+            if (this.finalJSONAr.length > 0) {
+                tempAr = this.finalJSONAr.pop().data;
             }
         }
-        for(i=0;i<arr.length;i++){
-            tempObj={};
-            tempObj['value']=arr[i]+'';
+        for (i = 0; i < arr.length; i++) {
+            tempObj = {};
+            tempObj['value'] = arr[i] + '';
             tempAr.push(tempObj);
         }
 
-        tempObj={};
-        tempObj['seriesname']='series';
-        tempObj['data']=tempAr;
+        tempObj = {};
+        tempObj['seriesname'] = 'series';
+        tempObj['data'] = tempAr;
 
         this.finalJSONAr.push(tempObj);
     }
 
-    parseDatasetBubble(arr,append){
-        let i, tempAr=[], tempObj={};
+    parseDatasetBubble(arr, append) {
+        let i, tempAr = [],
+            tempObj = {};
 
-        if(append===true){
-            if(this.finalJSONAr.length>0){
-                tempAr=this.finalJSONAr.pop().data;
+        if (append === true) {
+            if (this.finalJSONAr.length > 0) {
+                tempAr = this.finalJSONAr.pop().data;
             }
         }
 
-        for(i=0;i<arr.length;i++){
-            tempObj={};
-            tempObj['x']=arr[i][0]+'';
-            tempObj['y']=arr[i][1]+'';
-            tempObj['z']=arr[i][2]+'';
-            tempObj['name']='randomName';
+        for (i = 0; i < arr.length; i++) {
+            tempObj = {};
+            tempObj['x'] = arr[i][0] + '';
+            tempObj['y'] = arr[i][1] + '';
+            tempObj['z'] = arr[i][2] + '';
+            tempObj['name'] = 'randomName';
             tempAr.push(tempObj);
         }
 
-        tempObj={};
-        tempObj['seriesname']='Bubbles';
-        tempObj['data']=tempAr;
+        tempObj = {};
+        tempObj['seriesname'] = 'Bubbles';
+        tempObj['data'] = tempAr;
 
         this.finalJSONAr.push(tempObj);
     }
 
-    parseDatasetScatter(arr,append){
-        let i, tempAr=[], tempObj={};
+    parseDatasetScatter(arr, append) {
+        let i, tempAr = [],
+            tempObj = {};
 
-        if(append===true){
-            if(this.finalJSONAr.length>0){
-                tempAr=this.finalJSONAr.pop().data;
+        if (append === true) {
+            if (this.finalJSONAr.length > 0) {
+                tempAr = this.finalJSONAr.pop().data;
             }
         }
 
-        for(i=0;i<arr.length;i++){
-            tempObj={};
-            tempObj['y']=arr[i][0]+'';
-            tempObj['x']=arr[i][1]+'';
+        for (i = 0; i < arr.length; i++) {
+            tempObj = {};
+            tempObj['y'] = arr[i][0] + '';
+            tempObj['x'] = arr[i][1] + '';
             tempAr.push(tempObj);
         }
 
-        tempObj={};
-        tempObj['seriesname']='scatter';
-        tempObj['data']=tempAr;
+        tempObj = {};
+        tempObj['seriesname'] = 'scatter';
+        tempObj['data'] = tempAr;
 
         this.finalJSONAr.push(tempObj);
     }
 
-    addArray(arr,append){
-        if(arr===undefined || arr.length===0){
+    addArray(arr, append) {
+        // console.log(arr);
+        if (arr === undefined || arr.length === 0) {
             return 'Array cannot be undefined or empty';
         }
-        switch(this.chartType){
-            case 0: this.parseData(arr);break;
-            case 1: this.parseDataset(arr,append);break;
-            case 2: this.parseDatasetScatter(arr);break;
-            case 3: this.parseDatasetBubble(arr);break;
+        switch (this.chartType) {
+            case 0:
+                this.parseData(arr);
+                break;
+            case 1:
+                this.parseDataset(arr, append);
+                break;
+            case 2:
+                this.parseDatasetScatter(arr, append);
+                break;
+            case 3:
+                this.parseDatasetBubble(arr, append);
+                break;
             default: // do nothing.
         }
     }
 
-    generateData(numberType,total,append){
-        //
-        if(append){}
+    createNewPropertyOfNumber(property, _value) {
+        Object.defineProperty(NumberGenerator.prototype, property, {
+            value: _value
+        });
     }
-    getJSON(){
-        if(this.chartType===0){
-            this.finalJSON['data']=this.finalJSONAr;
-        }else if(this.chartType===1){
-            this.finalJSON['dataset']=this.finalJSONAr;
-        }else if(this.chartType===2){
-            this.finalJSON['dataset']=this.finalJSONAr;
-        }else if(this.chartType===3){
-            this.finalJSON['dataset']=this.finalJSONAr;
+
+    modifyNumber(property, value) {
+        numberGeneratorObj.modifier(property, value);
+    }
+    generateNumber(numberType, total, append) {
+        this.addArray(numberGeneratorObj.generate(numberType, total), append);
+    }
+    getJSON() {
+        if (this.chartType === 0) {
+            this.finalJSON['data'] = this.finalJSONAr;
+        } else if (this.chartType === 1) {
+            this.finalJSON['dataset'] = this.finalJSONAr;
+        } else if (this.chartType === 2) {
+            this.finalJSON['dataset'] = this.finalJSONAr;
+        } else if (this.chartType === 3) {
+            this.finalJSON['dataset'] = this.finalJSONAr;
         }
 
-        console.log(JSON.stringify(this.finalJSON,null,2));
+        console.log(JSON.stringify(this.finalJSON, null, 2));
         this.initialize();
-        return JSON.stringify(this.finalJSON,null,2);
+        return JSON.stringify(this.finalJSON, null, 2);
     }
 }
 
-const obj = new DataGenerator('mscolumn2d');
+const datageneratorObj = new DataGenerator('column2d');
 
-obj.addArray([1,2,3,4,5,6,7,7,8,9],false);
-obj.addArray([1,2,3,4,5,6,7,7,8,9],false);
-obj.getJSON();
+// Write your code here
+
+
+// datageneratorObj.modifyNumber('range','30, 200');
+// datageneratorObj.modifyNumber('trend','linear');
+// datageneratorObj.generateNumber('integer',5,false);
+
+// datageneratorObj.getJSON();
+
+// datageneratorObj.modifyNumber('range','300, 500');
+// datageneratorObj.modifyNumber('trend','random');
+// datageneratorObj.generateNumber('integer',5,true);
+
+// datageneratorObj.getJSON();
